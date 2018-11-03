57db59eb0ceb249f7ea5291106d4b75d62099dc3
Erin Dubuc <erin.dubuc9@gmail.com>
2018-11-03 15:24:03 +0000

    added jsonwebtoken to user login

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-	mongoURI: 'mongodb://erin:asdf1234@ds151293.mlab.com:51293/devconnector'	
+	mongoURI: 'mongodb://erin:asdf1234@ds151293.mlab.com:51293/devconnector',
+	secretOrKey: 'secret'
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 // Load User model
 const User = require('../../models/User');
@@ -48,6 +50,46 @@ router.post('/register', (req, res) => {
 		})
 		.catch();
 
+});
+
+// @route	GET api/users/login
+// @desc	Login user / Returning JSonWebToken
+// @access	Public
+router.post('/login', (req, res) => {
+	const email = req.body.email;
+	const password = req.body.password;
+	
+	// Find user by email
+	User.findOne({email})
+		.then(user => {
+			if(!user) {
+				return res.json(404).json({email: 'User not found'});
+			}
+			
+			// Check Password
+			bcrypt.compare(password, user.password)
+				.then(isMatch => {
+					if(isMatch) {
+						// User Matched
+						
+						const payload = { id: user.id, name: user.name, avatar: user.avatar } //Create JWT Payload
+						
+						// Sign Token
+						jwt.sign(
+							payload, 
+							keys.secretOrKey, 
+							{ expiresIn: 3600 },
+							(err, token) => {
+								res.json({
+								success: true,
+								token: 'Bearer ' + token
+								});
+							});
+					} else {
+						return res.status(400).json({password: 'Password incorrect'});
+					}
+				});
+		});
 });
 
 module.exports = router;
