484c1f94508c8483f5655412942a9e233435d3e1
Erin Dubuc <erin.dubuc9@gmail.com>
2018-11-11 16:24:41 +0000

    implemented React, added Navbar, Landing, Footer

--- server.js
+++ server.js
@@ -36,6 +36,7 @@ app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
 
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
+const port = 8081 || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
