63f96b478953a2a3752768ce92b37fe5cb37c709
Erin Dubuc <erin.dubuc9@gmail.com>
2018-11-25 23:04:18 -0500

    added setCurrentUser to login actions

---
+++ client/src/utils/setAuthToken.js
@@ -0,0 +1,14 @@
+import axios from 'axios';  // Prevents us from manually needing the token for login
+
+
+const setAuthToken = token => {
+  if (token) {
+    // Apply to every request
+    axios.defaults.header.common['Authorization'] = token;
+  } else {
+    // Delete auth header
+    delete axios.defaults.headers.common['Authorization'];
+  }
+};
+
+export default setAuthToken;
