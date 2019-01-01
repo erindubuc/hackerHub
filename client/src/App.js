5935c920a8ee6e15a9e01dd9a8b6ca66991fe1db
Erin Dubuc <erin.dubuc9@gmail.com>
2019-01-01 09:02:25 -0500

    created addEducation and gave it functionality

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,7 @@ import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/create-profile/CreateProfile';
 import EditProfile from './components/edit-profile/EditProfile';
 import AddExperience from './components/add-credentials/AddExperience';
+import AddEducation from './components/add-credentials/AddEducation';
 
 // Check for token - if it exists...
 if (localStorage.jwtToken) {
@@ -70,6 +71,11 @@ class App extends Component {
                 <PrivateRoute
                   exact path="/add-experience"
                   component={AddExperience} />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact path="/add-education"
+                  component={AddEducation} />
               </Switch>
             </div>
             <Footer />
