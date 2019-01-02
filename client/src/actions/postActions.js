43a23b7ed8840c12845bafc87b1117bed60ce250
Erin Dubuc <erin.dubuc9@gmail.com>
2019-01-02 18:31:39 -0500

    created PostFeed Component

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 import {
   ADD_POST,
-  GET_ERRORS
+  GET_ERRORS,
+  GET_POSTS,
+  POST_LOADING
 } from './types';
 
 // Add post
@@ -22,3 +24,29 @@ export const addPost = postData => dispatch => {
       })
     );
 };
+
+// Get posts
+export const getPosts = () => dispatch => {
+  dispatch(setPostLoading());
+  axios
+    .post('/api/posts')
+    .then(res =>
+      dispatch({
+        type: GET_POSTS,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_POSTS,
+        payload: null
+      })
+    );
+};
+
+// Set loading state
+export const setPostLoading = () => {
+  return {
+    type: POST_LOADING
+  };
+}; 
