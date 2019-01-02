54e0b4b39348d41a736190e3c9f8f4540ff4710b
Erin Dubuc <erin.dubuc9@gmail.com>
2019-01-02 15:01:00 -0500

    created postReducer, and PostForm works

--- client/src/App.js
+++ client/src/App.js
@@ -24,6 +24,7 @@ import AddEducation from './components/add-credentials/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
 import NotFound from './components/not-found/NotFound';
+import Posts from './components/posts/Posts';
 
 // Check for token - if it exists...
 if (localStorage.jwtToken) {
@@ -81,6 +82,11 @@ class App extends Component {
                 <PrivateRoute
                   exact path="/add-education"
                   component={AddEducation} />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact path="/feed"
+                  component={Posts} />
               </Switch>
               <Route exact path="/not-found" component={NotFound} />
             </div>
