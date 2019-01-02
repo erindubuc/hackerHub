fb2a150efaa825effea8a010b2e1722d23563555
Erin Dubuc <erin.dubuc9@gmail.com>
2019-01-01 19:50:06 -0500

    AddEducation working on Dashboard

--- client/src/components/add-credentials/AddEducation.js
+++ client/src/components/add-credentials/AddEducation.js
@@ -124,7 +124,7 @@ class AddEducation extends Component {
                     name="current"
                     value={this.state.current}
                     checked={this.state.current}
-                    onChange={this.state}
+                    onChange={this.onCheck}
                     id="current"
                   />
                   <label htmlFor="current" className="form-check-label">
