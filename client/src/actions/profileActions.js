a4133ce62880f5936c743deb3bd5e01662d5c277
Erin Dubuc <erin.dubuc9@gmail.com>
2019-01-01 23:14:14 -0500

    created Profile Component and files for each section

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -25,7 +25,26 @@ export const getCurrentProfile = () => dispatch => {
         payload: {}
       })
     );
-}
+};
+
+// Get profile by handle
+export const getProfileByHandle = (handle) => dispatch => {
+  dispatch(setProfileLoading());
+  axios
+    .get(`/api/profile/handle/${handle}`)
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: null
+      })
+    );
+};
 
 // Create Profile
 export const createProfile = (profileData, history) => dispatch => {
