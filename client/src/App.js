ff94de88740cd16709a918b0fa241cca3154bbe2
Erin Dubuc <erin.dubuc9@gmail.com>
2019-01-02 22:36:47 -0500

    Post Component created

--- client/src/App.js
+++ client/src/App.js
@@ -25,6 +25,7 @@ import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
 import NotFound from './components/not-found/NotFound';
 import Posts from './components/posts/Posts';
+import Post from './components/post/Post';
 
 // Check for token - if it exists...
 if (localStorage.jwtToken) {
@@ -87,6 +88,11 @@ class App extends Component {
                 <PrivateRoute
                   exact path="/feed"
                   component={Posts} />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact path="/post/:id"
+                  component={Post} />
               </Switch>
               <Route exact path="/not-found" component={NotFound} />
             </div>
