d63f1ee7c08dc6fc1a0030eedc2b5492eee8a30d
Erin Dubuc <erin.dubuc9@gmail.com>
2019-01-02 21:28:10 -0500

    deletePost works

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -4,7 +4,9 @@ import {
   ADD_POST,
   GET_ERRORS,
   GET_POSTS,
-  POST_LOADING
+
+  POST_LOADING,
+  DELETE_POST
 } from './types';
 
 // Add post
@@ -29,7 +31,7 @@ export const addPost = postData => dispatch => {
 export const getPosts = () => dispatch => {
   dispatch(setPostLoading());
   axios
-    .post('/api/posts')
+    .get('/api/posts')
     .then(res =>
       dispatch({
         type: GET_POSTS,
@@ -40,6 +42,24 @@ export const getPosts = () => dispatch => {
       dispatch({
         type: GET_POSTS,
         payload: null
+      })
+    );
+};
+
+// Delete post
+export const deletePost = id => dispatch => {
+  axios
+    .delete(`/api/posts/${id}`)
+    .then(res =>
+      dispatch({
+        type: DELETE_POST,
+        payload: id
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
       })
     );
 };
