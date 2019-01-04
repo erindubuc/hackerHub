fd092f9d73329839d6c65e8cc07cba66d1f64dd9
Erin Dubuc <erin.dubuc9@gmail.com>
2019-01-04 15:09:02 -0500

    Ready to deploy

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -50,7 +50,7 @@ class Dashboard extends Component {
             <p className="lead text-muted">Welcome {user.name}</p>
             <p>You have not yet setup a profile, please add some info</p>
             <Link to="/create-profile" className="btn btn-lg btn-info">
-              Create Profile
+              Create Your Profile
             </Link>
           </div>
         )
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -84,7 +84,7 @@ class Navbar extends Component {
 			<nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4" >
 				<div className="container">
 					<Link className="navbar-brand" to="/" role="tab">
-						DevConnector
+						hackerHub
 					</Link>
 					<button
 						className="navbar-toggler"
@@ -100,7 +100,7 @@ class Navbar extends Component {
 							<li className="nav-item">
 								<Link className="nav-link" to="/profiles" role="tab">
 									{' '}
-									Developers
+									Hackers
 								</Link>
 							</li>
 						</ul>
