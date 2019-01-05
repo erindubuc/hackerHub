e22e11884c6326119699c14e94bc037d2c310e82
Erin Dubuc <erin.dubuc9@gmail.com>
2019-01-05 00:11:56 -0500

    Prepared to deploy

--- server.js
+++ server.js
@@ -35,6 +35,16 @@ app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
+// Server static assests if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder 
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const port = process.env.PORT || 5000;
 // const port = 8081 || 5000;
 
